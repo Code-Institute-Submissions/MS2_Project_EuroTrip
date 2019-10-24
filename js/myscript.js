6c4eccbb4127b4483b24b1f1c688d101e3c86647
brianscan14 <brianscanlon14@gmail.com>
2019-10-24 17:07:50 +0000

    tidying up code, i am after adding all the markers individually to avoid clustering, need to add correct filter function now

--- js/myscript.js
+++ js/myscript.js
@@ -23,30 +23,12 @@ function initMap() {
 function setMarkers(map) {
   // Adds markers to the map.
 
-  // Marker sizes are expressed as a Size of X,Y where the origin of the image
-  // (0,0) is located in the top left of the image.
-
-  // Origins, anchor positions and coordinates of the marker increase in the X
-  // direction to the right and in the Y direction down.
-  var image = {
-    url: 'https://developers.google.com/maps/documentation/javascript/examples/full/images/beachflag.png',
-    // This marker is 20 pixels wide by 32 pixels high.
-    size: new google.maps.Size(20, 32),
-    // The origin for this image is (0, 0).
-    origin: new google.maps.Point(0, 0),
-    // The anchor for this image is the base of the flagpole at (0, 32).
-    anchor: new google.maps.Point(0, 32)
-  };
-  // Shapes define the clickable region of the icon. The type defines an HTML
-  // <area> element 'poly' which traces out a polygon as a series of X,Y points.
-  // The final coordinate closes the poly by connecting to the first coordinate.
-
   for (var i = 0; i < locations.length; i++) {
     var location = locations[i];
     var marker = new google.maps.Marker({
       position: {lat: location[1], lng: location[2]},
       map: map,
-      icon: image,
+      
       title: location[0],
     });
   }
